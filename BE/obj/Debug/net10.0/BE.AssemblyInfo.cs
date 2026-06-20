abdad4c2b0462e2b9874620f295ddd32f6fb11d8
Tran_Nhu <tranquynhnhu102025@gmail.com>
2026-06-21 02:32:32 +0700

    Dọn sạch rác trong dự án, loại bỏ file tạm

--- BE/obj/Debug/net10.0/BE.AssemblyInfo.cs
+++ BE/obj/Debug/net10.0/BE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48caf25d60ba4deb766ab5c4ff034731c22f070b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d58dfabda0d93003f30bfe7411f3968cb52ea44")]
 [assembly: System.Reflection.AssemblyProductAttribute("BE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
